051fba94b50e4c28e2e26f3f84f4c074f5132e9f
Payson Wallach <payson@paysonwallach.com>
2021-07-20 21:28:32 +0200

    feat: Selectively display menu items

--- source/Background/index.ts
+++ source/Background/index.ts
@@ -8,9 +8,14 @@ import {
 import { v4 as uuidv4 } from "uuid"
 import { serialize, deserialize } from "typescript-json-serializer"
 import yaml from "js-yaml"
+import { from } from "rxjs"
+import { map, pairwise } from "rxjs/operators"
+
+import { configureStore, createSlice } from "@reduxjs/toolkit"
+import { PayloadAction } from "@reduxjs/toolkit/dist/createAction"
 
 import { Lazy } from "Utils/Lazy"
-import { Backend, ISession } from "Common/Backend"
+import { Backend } from "Common/Backend"
 import { Config } from "Common/Config"
 import logs from "Common/Logging"
 import {
@@ -23,8 +28,131 @@ import {
     UpdateSessionRequest,
 } from "Common/Protocol"
 
+interface SessionsState {
+    sessionIds: number[]
+    activeSessionId?: number
+}
+
 const backend = new Backend()
 
+const initialState = {
+    sessionIds: [] as number[],
+    activeSessionId: undefined,
+} as SessionsState
+
+const sessionSlice = createSlice({
+    name: "sessions",
+    initialState,
+    reducers: {
+        sessionAdded(state: SessionsState, action: PayloadAction<number>) {
+            state.activeSessionId = action.payload
+            state.sessionIds.push(action.payload)
+        },
+        sessionRemoved(state: SessionsState, action: PayloadAction<number>) {
+            const index = state.sessionIds.indexOf(action.payload, 0)
+            if (index > -1) state.sessionIds.splice(index, 1)
+        },
+        activeSessionChanged(
+            state: SessionsState,
+            action: PayloadAction<number>
+        ) {
+            state.activeSessionId = action.payload
+        },
+    },
+})
+const { sessionAdded, sessionRemoved, activeSessionChanged } =
+    sessionSlice.actions
+
+const store = configureStore({
+    reducer: { sessions: sessionSlice.reducer },
+})
+
+const createSessionMenuItem = (id: number, name: string) => {
+    browser.menus.create({
+        id: `${id}`,
+        title: name,
+        contexts: ["tab"],
+        parentId: "move-to-session",
+        onclick: onSessionMenuItemClicked,
+    })
+}
+
+const onSessionMenuItemClicked = async (
+    info: Menus.OnClickData,
+    tab: Tabs.Tab
+) => {
+    try {
+        const tabs = await browser.tabs.query({
+            highlighted: true,
+            currentWindow: true,
+        })
+        const tabIds: number[] = tabs
+            .filter((tab) => tab.id !== undefined)
+            .map((tab) => tab.id!)
+        const windowId =
+            typeof info.menuItemId === "string"
+                ? parseInt(info.menuItemId)
+                : info.menuItemId
+
+        if (windowId !== undefined)
+            browser.tabs.move(tabIds, { windowId: windowId, index: -1 })
+    } catch (error) {
+        logs.error(error)
+    }
+}
+
+const difference = (setA: Set<number>, setB: Set<number>) => {
+    const _difference = new Set(setA)
+
+    for (const member of setB) _difference.delete(member)
+
+    return _difference
+}
+
+const stateSubscription = from(store)
+    .pipe(
+        map((state) => {
+            return state.sessions.sessionIds.filter(
+                (id) => id != state.sessions.activeSessionId
+            )
+        }),
+        pairwise()
+    )
+    .subscribe(async (state) => {
+        if (state[1].lenth <= 1) {
+            browser.menus.remove("move-to-session")
+            return
+        } else if (state[0].length <= 1 && state[1].length > 1) {
+            browser.menus.create({
+                id: "move-to-session",
+                title: "Move to Session",
+                contexts: ["tab"],
+            })
+        }
+
+        const itemsToAdd =
+            state[0].length > 1 ? difference(state[1], state[0]) : state[1]
+        for (const item of itemsToAdd) {
+            const session = await backend.sessions
+                .where({ windowId: item })
+                .first()
+            if (session) createSessionMenuItem(item, session.name)
+            else logs.error(`unable to find session for window ${item}`)
+        }
+
+        const itemsToRemove = difference(state[0], state[1])
+        for (const item of itemsToRemove) browser.menus.remove(`${item}`)
+    })
+
+browser.windows.onFocusChanged.addListener((windowId) => {
+    if (windowId != browser.windows.WINDOW_ID_NONE) {
+        store.dispatch(activeSessionChanged(windowId))
+    }
+})
+
+backend.sessions.each((session) =>
+    store.dispatch(sessionAdded(session.windowId))
+)
 const pendingSessions = new Map<string, string>()
 
 const hostConnector = new Lazy<Runtime.Port>(() =>
@@ -208,49 +336,8 @@ const onSessionChanged = (windowId: number, tabId: number | null = null) => {
         )
 }
 
-const onSessionMenuItemClicked = (info: Menus.OnClickData, tab: Tabs.Tab) => {
-    browser.tabs.query({ highlighted: true, currentWindow: true }).then(
-        (tabs) => {
-            const tabIds: number[] = tabs
-                .filter((tab) => tab.id !== undefined)
-                .map((tab) => tab.id!)
-            const windowId =
-                typeof info.menuItemId === "string"
-                    ? parseInt(info.menuItemId)
-                    : info.menuItemId
-
-            if (windowId !== undefined)
-                browser.tabs.move(tabIds, { windowId: windowId, index: -1 })
-        },
-        (error) => {
-            logs.error(error)
-        }
-    )
-}
-
-browser.menus.create({
-    id: "move-to-session",
-    title: "Move to session",
-    contexts: ["tab"],
-})
-backend.sessions.each((session: ISession) => {
-    browser.menus.create({
-        id: `${session.windowId}`,
-        title: `${session.name}`,
-        contexts: ["tab"],
-        parentId: "move-to-session",
-        onclick: onSessionMenuItemClicked,
-    })
-})
-
-    browser.menus.create({
-        id: `${obj.windowId}`,
-        title: obj.name ?? `Session ${obj.id}`,
-        contexts: ["tab"],
-        parentId: "move-to-session",
-        onclick: onSessionMenuItemClicked,
-    })
 backend.sessions.hook("creating", (primKey, obj, transaction) => {
+    store.dispatch(sessionAdded(obj.windowId))
 })
 backend.sessions.hook("updating", (mods: any, primKey, obj, trans) => {
     if (mods.hasOwnProperty("name")) {
@@ -261,8 +348,8 @@ backend.sessions.hook("updating", (mods: any, primKey, obj, trans) => {
         }
     }
 })
-    browser.menus.remove(`${obj.windowId}`)
 backend.sessions.hook("deleting", (primKey, obj, transaction) => {
+    store.dispatch(sessionRemoved(obj.windowId))
 })
 
 browser.windows.onRemoved.addListener((windowId) => {
